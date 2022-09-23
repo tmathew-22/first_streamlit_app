950c76a17f8d13d8b9fa5ce2e7d41312368095cb
Thomas Mathew <113944722+tmathew-22@users.noreply.github.com>
2022-09-23 09:37:59 -0500

    Update fruit_macros.py

--- fruit_macros.py
+++ fruit_macros.py
@@ -1,3 +1,2 @@
-import pandas 
-my _fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-streamlit.dataframe(my_fruit_list)
+import streamlit
+streamlit.title("My Mom's New Healthy Diner")
